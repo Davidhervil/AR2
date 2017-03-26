bb3c265d139508b91c283deb916c45a7f43c417d
jemd93 <jemd311@gmail.com>
2017-03-25 21:16:21 -0400

    Pusheando el juego

---
+++ Juego/Proyecto2RA/Assets/VirtualButTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.SceneManagement;
+
+public class VirtualButTest : MonoBehaviour, IVirtualButtonEventHandler {
+
+    private GameObject Cubo;
+	// Use this for initialization
+	void Start () {
+        Cubo = GameObject.FindWithTag("Player");
+        VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
+        for (int  i = 0; i < vbs.Length; ++i)
+        {
+            vbs[i].RegisterEventHandler(this);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnButtonPressed(VirtualButtonAbstractBehaviour vb)
+    {
+        switch(vb.VirtualButtonName)
+        {
+            case "StartButton":
+                SceneManager.LoadScene("Level1");
+                break;
+            default:
+                throw new UnityException("Error en el boton");
+                break;
+        }
+    }
+
+    public void OnButtonReleased(VirtualButtonAbstractBehaviour vb)
+    {
+        switch (vb.VirtualButtonName)
+        {
+            case "StartButton":
+                SceneManager.LoadScene("Level1");
+                break;
+            case "ExitButton":
+                Application.Quit();
+                break;
+            default:
+                throw new UnityException("Error en el boton");
+                break;
+        }
+    }
+}
