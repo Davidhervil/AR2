9f3d5996f7a3291ebf5fe75c38a67782567699e5
jemd93 <jemd311@gmail.com>
2017-03-27 16:08:12 -0400

    Pusheando movimiento teclado y colliders

---
+++ Juego/Proyecto2RA/Assets/MoveScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour {
+
+    public float speed = 15.0f;
+    public float rotation = 5.0f;
+   
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -rotation, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, rotation, 0);
+        }
+	}
+}
