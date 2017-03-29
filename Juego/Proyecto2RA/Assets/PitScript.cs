90813cec8cf9973199d535b0828b1dddabfed3bf
jemd93 <jemd311@gmail.com>
2017-03-28 21:26:46 -0400

    Pusheando

---
+++ Juego/Proyecto2RA/Assets/PitScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.SceneManagement;
+
+public class PitScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene("Level2");
+        }
+    }
+}
