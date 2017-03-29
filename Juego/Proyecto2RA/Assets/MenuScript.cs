5b08cd45aab17ef709fc3b2d0fa9cd593a08526a
jemd93 <jemd311@gmail.com>
2017-03-28 21:09:15 -0400

    Pusheando menus

---
+++ Juego/Proyecto2RA/Assets/MenuScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+    public Color OnMouseOverColor = Color.yellow;
+    public Color OnMouseClickColor = Color.red;
+    public Color OnMouseExitColor = Color.cyan;
+
+    void Start()
+    {
+
+    }
+
+    void OnMouseOver()
+    {
+        GetComponent<TextMesh>().color = OnMouseOverColor;
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<TextMesh>().color = OnMouseClickColor;
+            if (transform.tag == "BackToMenu")
+                SceneManager.LoadScene("Menu");
+            else if (transform.tag == "ExitText")
+                Application.Quit();
+            else if (transform.tag == "Play1")
+                SceneManager.LoadScene("Level1");
+            else if (transform.tag == "Play2")
+                SceneManager.LoadScene("Level2");
+        }
+    }
+    void OnMouseExit()
+    {
+        GetComponent<TextMesh>().color = Color.cyan;
+    }
+
+
+}
